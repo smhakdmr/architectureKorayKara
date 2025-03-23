7369368b5017dd0eb3845098424d338b1bc488aa
smhakdmr <ilyassemihakdemir@gmail.com>
2025-03-23 17:25:41 +0300

    Anasayfa ekran ortalama sorunları çözüm çalışması v4.

--- src/app/components/WelcomeBox.jsx
+++ src/app/components/WelcomeBox.jsx
@@ -28,36 +28,39 @@ const WelcomeBox = () => {
 
     return (
         <Container maxWidth="lg" sx={{ margin: 'auto' }}>
-            <Box
-                sx={{
-                    position: "relative",
-                    background: `url('/mimarlik4.png')`,
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center',
-                    width: '80vw',
-                    height: '60vh',
-                    justifySelf: "center",
-                    marginTop: scrolled ? '30vh' : '3vh'
-                }}>
+            <Box sx={{ padding: { xs: 2, sm: 4 } }}>
                 <Box
                     sx={{
-                        position: 'absolute',
-                        bottom: isSmallScreen ? '15px' : '50px',
-                        left: isSmallScreen ? '15px' : '50px',
-                        backgroundColor: 'white',
-                        padding: '40px',
-                        boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
-                        width: isSmallScreen ? '300px' : '20vw',
-                        height: isSmallScreen ? '30vh' : '35vh'
-                    }}
-                >
-                    <Typography variant="h6" sx={{ fontWeight: 'bold', marginBottom: '10px', fontFamily: "inherit" }}>
-                        Tasarım Mimarlık
-                    </Typography>
-                    <Typography variant={isSmallScreen ? "body2" : "body3"} sx={{ fontFamily: "inherit" }}>
-                        Tasarım Mimarlık, kentsel tasarım kollarında yenilikçi ve ilerici bir düşünce ve zihniyet anlayışını işbirlikçi
-                        tasarım uygulamalarıyla yakalama arayışı içinde 2013 yılında kurulmuştur.
-                    </Typography>
+                        position: "relative",
+                        background: `url('/mimarlik4.png')`,
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        width: '80vw',
+                        height: '60vh',
+                        justifySelf: "center",
+                        marginTop: scrolled ? '30vh' : '3vh',
+                        justifyContent: 'center'
+                    }}>
+                    <Box
+                        sx={{
+                            position: 'absolute',
+                            bottom: isSmallScreen ? '15px' : '50px',
+                            left: isSmallScreen ? '15px' : '50px',
+                            backgroundColor: 'white',
+                            padding: '40px',
+                            boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+                            width: isSmallScreen ? '300px' : '20vw',
+                            height: isSmallScreen ? '30vh' : '35vh'
+                        }}
+                    >
+                        <Typography variant="h6" sx={{ fontWeight: 'bold', marginBottom: '10px', fontFamily: "inherit" }}>
+                            Tasarım Mimarlık
+                        </Typography>
+                        <Typography variant={isSmallScreen ? "body2" : "body3"} sx={{ fontFamily: "inherit" }}>
+                            Tasarım Mimarlık, kentsel tasarım kollarında yenilikçi ve ilerici bir düşünce ve zihniyet anlayışını işbirlikçi
+                            tasarım uygulamalarıyla yakalama arayışı içinde 2013 yılında kurulmuştur.
+                        </Typography>
+                    </Box>
                 </Box>
             </Box>
         </Container>
