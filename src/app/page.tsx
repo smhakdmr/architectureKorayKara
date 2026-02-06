0f91dbf6742a521385e6f9636f386848fd77edba
smhakdmr <ilyassemihakdemir@gmail.com>
2026-02-07 02:56:54 +0300

    mobil scroll bug fix.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Projects from './projects/page'
 import ContactUs from './contactus/page'
 import { Box } from '@mui/material';
 import WelcomeBox from "../app/components/WelcomeBox";
-import ScrollReveal from "../app/components/ScrollReveal";
 
 export default function Home() {
 
@@ -16,17 +15,13 @@ export default function Home() {
         <div className="section-divider" />
       </Box>
 
-      <ScrollReveal direction="up" delay={0.1}>
-        <Projects />
-      </ScrollReveal>
+      <Projects />
 
       <Box sx={{ display: 'flex', justifyContent: 'center', py: { xs: 5, sm: 6 } }}>
         <div className="section-divider" />
       </Box>
 
-      <ScrollReveal direction="up" delay={0.15}>
-        <ContactUs />
-      </ScrollReveal>
+      <ContactUs />
     </main>
   );
 }
